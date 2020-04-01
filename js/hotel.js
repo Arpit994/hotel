fc7990cc54d3a7e29ae782f420bf4376dc6eb2b0
Arpit <arpitkumar994@gmail.com>
2020-04-01 17:41:16 +0530

    hi

---
+++ js/hotel.js
@@ -0,0 +1,90 @@
+ 
+       	        $(document).ready(function(){
+       	        	$('.imgtwo').hover(function(){
+                        $('.rate1').html('Deluxe Room <br/>$199<br/> Size:  30ft<br/> Capactity: 2 Person<br/> Bed: Single<br/>Service: Bathroom, Bed, Ac<br/> ');
+                        $('.rate1').css('top','10%');
+                       
+                        $('.rate1').css('transition','1s');
+                        $('.rate1').css('background-color','rgba(0,0,0,0.8)');
+                       
+                     
+                       
+       	        	});
+       	        	$('.imgtwo').mouseleave(function(){
+                        $('.rate1').html('Deluxe Room <br/>$199<sub>/per night</sub>');
+                        $('.rate1').css('top','70%');
+                                                      
+                        $('.rate1').css('background-color','red');
+                       
+       	        	});
+       	        });
+
+
+
+               $(document).ready(function(){
+       	        	$('.imgone').hover(function(){
+                        $('.rate2').html('Deluxe Room <br/>$199<br/> Size:  30ft<br/> Capactity: 2 Person<br/> Bed: Single<br/>Service: Bathroom, Bed, Ac<br/> ');
+                        $('.rate2').css('top','10%');
+                       
+                        $('.rate2').css('transition','1s');
+                        $('.rate2').css('background-color','rgba(0,0,0,0.8)');
+                        
+                      
+                       
+       	        	});
+       	        	$('.imgone').mouseleave(function(){
+                        $('.rate2').html('Family Room <br/>$250<sub>/per night</sub>');
+                        $('.rate2').css('top','70%');
+                        
+                        $('.rate2').css('background-color','red');
+                       
+       	        	});
+       	        });
+              
+
+
+               $(document).ready(function(){
+       	        	$('.imgfour').hover(function(){
+                        $('.rate3').html('Deluxe Room <br/>$199<br/> Size:  30ft<br/> Capactity: 2 Person<br/> Bed: Single<br/>Service: Bathroom, Bed, Ac<br/> ');
+                        $('.rate3').css('top','10%');
+                        
+                        $('.rate3').css('transition','1s');
+                        $('.rate3').css('background-color','rgba(0,0,0,0.8)');
+                        
+                        
+                       
+                       
+       	        	});
+       	        	$('.imgfour').mouseleave(function(){
+                        $('.rate3').html('Single Room <br/>$149<sub>/per night</sub>');
+                        $('.rate3').css('top','70%');
+                     
+                        $('.rate3').css('background-color','red');
+                       
+       	        	});
+       	        });
+
+
+
+                $(document).ready(function(){
+       	        	$('.imgthree').hover(function(){
+                        $('.rate4').html('Deluxe Room <br/>$199<br/> Size:  30ft<br/> Capactity: 2 Person<br/> Bed: Single<br/>Service: Bathroom, Bed, Ac<br/> ');
+                        $('.rate4').css('top','10%');
+                      
+                        $('.rate4').css('transition','1s');
+                        $('.rate4').css('background-color','rgba(0,0,0,0.8)');
+                      
+                      
+                       
+                       
+       	        	});
+       	        	$('.imgthree').mouseleave(function(){
+                        $('.rate4').html('Premium Deluxe Room <br/>$299<sub>/per night</sub>');
+                        $('.rate4').css('top','70%');
+                      
+                        $('.rate4').css('background-color','red');
+                       
+       	        	});
+       	        });
+       	       
+       
